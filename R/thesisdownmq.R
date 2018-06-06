147f24faf7f17beede6fec7bf05ac50e41c1eaae
thomas-fung <thomas.fung@mq.edu.au>
2018-06-06 14:32:05 +1000

    Initial files

---
+++ R/thesisdownmq.R
@@ -0,0 +1,15 @@
+#' thesisdownmq: A package for creating HDR theses using R Markdown 
+#'
+#'
+#' @section thesis_gitbook:
+#' Creates an R Markdown thesis template as a webpage (great for writing)
+#' @section thesis_pdf:
+#' Creates an R Markdown thesis template as a PDF (looks awesome)
+#' @section thesis_word:
+#' Creates an R Markdown thesis template as a Microsoft Word document (looks
+#' awful, not recommended) 
+#' @section thesis_epub:
+#' Creates an R Markdown thesis template as an ebook (looks awful, not recommended)
+#' @docType package
+#' @name thesisdownmq
+NULL
